8b21396f630555d3ef55536e777dd065345f5db6
prog-coder24 <vaishnavisawant1224@gmail.com>
2021-02-07 21:44:45 +0530

    added users.html

--- BS_APP/urls.py
+++ BS_APP/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home),
+    path('users/',user)
 ]
--- BS_APP/views.py
+++ BS_APP/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def user(request):
+
+    users = User.objects.all()
+    return render(request, 'users.html',{"users": users})
