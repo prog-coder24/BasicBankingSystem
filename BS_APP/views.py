eba716455dfb520f18ab85bbf005e641ec5540ea
prog-coder24 <70955462+prog-coder24@users.noreply.github.com>
2021-02-16 17:22:53 +0530

    Update views.py

--- BS_APP/views.py
+++ BS_APP/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import redirect
 
 # Create your views here.
 
-
 def home(request):
     return render(request,'home.html')
 
@@ -22,29 +21,45 @@ def transfer(request, pk):
     
 def history(request):
 
-    transactions = Transaction.objects.all()
+    transactions = Transaction.objects.order_by('-created_at')
     return render(request, 'history.html', {"transactions":transactions})
 
-def add_transfer(request):
+def add_transfer(request, pk):
 
      if request.method == 'POST':
 
          sender = request.POST.get('sender',None)
          receiver = request.POST.get('receiver',None)
          amount = request.POST.get('amount',None)
+
+         if int(amount) <= 0:
+             user = User.objects.get(pk=pk)
+             receiver = User.objects.all()
+             return render(request,'transfer.html',{"warning": "Oops! Entered amount is not valid", "receiver":receiver, "user": user})
+
          sender_instance = User.objects.get(full_name=sender)
          curr_balance = sender_instance.current_balance
          receiver_instance = User.objects.get(full_name=receiver)
         
          if curr_balance > int(amount):
-              status = 'Successful'
-              Transaction.objects.create(sender=sender_instance,recipient=receiver_instance,amount=amount,status=status)
-              return redirect(history)
+             status = 'Successful'
+
+             receiver_instance.current_balance += int(amount)
+
+             receiver_instance.save()
+
+             sender_instance.current_balance -= int(amount)
+
+             sender_instance.save()
+
+             Transaction.objects.create(sender=sender_instance,recipient=receiver_instance,amount=amount,status=status)
+             return redirect(history)
          else:
               status = 'Failed'
               Transaction.objects.create(sender=sender_instance,recipient=receiver_instance,amount=amount,status=status)
+              user = User.objects.get(pk=pk)
               receiver = User.objects.all()
-              return render(request,'transfer.html',{"warning": "Transaction failed because of insufficient balance :(", "receiver":receiver})
+              return render(request,'transfer.html',{"warning": "Transaction failed because of insufficient balance :(", "receiver":receiver, "user": user})
            
 
 
