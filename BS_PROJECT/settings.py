b20dd003fb89c7cd8601998358b973342cd25a65
prog-coder24 <vaishnavisawant1224@gmail.com>
2021-02-17 11:38:06 +0530

    added requirements.txt

--- BS_PROJECT/settings.py
+++ BS_PROJECT/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8j63l1z7uxkus-%w$0e2r*oa2##4*@l8%g2hpsscu0dxb)+8mp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0. 0.1']
 
 AUTH_USER_MODEL = 'BS_APP.User'
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- BS_PROJECT/urls.py
+++ BS_PROJECT/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('BS_APP.urls')),
+    path('', include('BS_APP.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
